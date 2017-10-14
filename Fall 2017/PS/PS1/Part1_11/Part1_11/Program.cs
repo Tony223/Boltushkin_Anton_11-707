accb710335b8f32829fa0ef0059a675119c22e00
Boltushkin Anton <32344640+Tony223@users.noreply.github.com>
2017-10-15 00:43:30 +0300

    Add files via upload

---
+++ Fall 2017/PS/PS1/Part1_11/Part1_11/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Part1_11
+{
+    class Program
+    {
+
+        public static double Area(double x1, double y1, double x2, double y2, double x3, double y3)
+        {
+            double sideA = Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+            double sideB = Math.Sqrt((x2 - x3) * (x2 - x3) + (y2 - y3) * (y2 - y3));
+            double sideC = Math.Sqrt((x1 - x3) * (x1 - x3) + (y1 - y3) * (y1 - y3));
+            double semiperimeter = ((sideA + sideB + sideC) / 2);
+            double area = Math.Sqrt(semiperimeter * (semiperimeter - sideA) * (semiperimeter - sideB) * (semiperimeter - sideC));
+            return area;
+        }
+
+
+        static void Main()
+        {
+            Console.WriteLine("Введите абсциссу первой точки");
+            double x1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите ординату первой точки");
+            double y1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите абсциссу второй точки");
+            double x2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите ординату второй точки");
+            double y2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите абсциссу третьей точки");
+            double x3 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите ординату третьей точки");
+            double y3 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Площадь треугольника = {Area(x1, y1, x2, y2, x3, y3)}");
+
+        }
+
+    }
+}
---
+++ Fall 2017/PS/PS1/Part1_20/Part1_20/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Part1_20
+{
+    class Program
+    {
+
+        public static string HappyTicket(int a1, int b1, int c1, int d1, int e1, int f1, int a2, int b2, int c2, int d2, int e2, int f2)
+        {
+            int a3 = a1 + a2;
+            int b3 = b1 + b2;
+            int c3 = c1 + c2;
+            int d3 = d1 + d2;
+            int e3 = e1 + e2;
+            int f3 = f1 + f2;
+            string result;
+
+            a3 %= 10;
+            b3 %= 10;
+            c3 %= 10;
+            d3 %= 10;
+            e3 %= 10;
+            f3 %= 10;
+
+            return result = a3 + b3 + c3 != d3 + e3 + f3 ? "Несчастливый билет" : "Счастливый билет";
+
+        }
+
+
+        static void Main()
+        {
+            Console.WriteLine("Введите первую цифру первого билета");
+            int a1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите вторую цифру первого билета");
+            int b1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите третью цифру первого билета");
+            int c1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите четвертую цифру первого билета");
+            int d1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите пятую цифру первого билета");
+            int e1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите шестую цифру первого билета");
+            int f1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите первую цифру второго билета");
+            int a2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите вторую цифру второго билета");
+            int b2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите третью цифру второго билета");
+            int c2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите четвертую цифру второго билета");
+            int d2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите пятую цифру второго билета");
+            int e2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите шестую цифру второго билета");
+            int f2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(HappyTicket(a1, b1, c1, d1, e1, f1, a2, b2, c2, d2, e2, f2));
+        }
+
+    }
+}
