cdffcddc2a19a567d02d70dfb3c6a05bfc25a1be
Boltushkin Anton <32344640+Tony223@users.noreply.github.com>
2017-10-15 00:47:17 +0300

    Add files via upload

---
+++ Fall 2017/PS/PS1/Part3_10/Part3_10/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Part3_10
+{
+    class Program
+    {
+        static void Main()
+        {
+            int sum = 0;
+            double previousNumber = 0;
+
+            for (; ; )
+            {
+                double number = double.Parse(Console.ReadLine());
+                number = number / 1000000000;
+
+                if (number == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    if ((number > 0 && previousNumber >= 0) || (number < 0 && previousNumber <= 0))
+                    {
+                        previousNumber = number;
+                    }
+                    else
+                    {
+                        previousNumber = number;
+                        sum = sum + 1;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Был введен 0. Знак сменился {sum} раз");
+        }
+    }
+}
---
+++ Fall 2017/PS/PS1/Part3_11/Part3_11/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Part3_11
+{
+    class Program
+    {
+        static void Main()
+        {
+            double number = double.Parse(Console.ReadLine());
+            double maxNumber = number, minNumber = number;
+            for (; ; )
+            {
+                if (number == 0)
+                {
+                    break;
+                }
+
+                if (number > maxNumber)
+                {
+                    maxNumber = number;
+                }
+                if (number < minNumber)
+                {
+                    minNumber = number;
+                }
+                number = double.Parse(Console.ReadLine());
+            }
+            Console.WriteLine($"Минимальное число = {minNumber}, максимальное число = {maxNumber}");
+        }
+    }
+}
