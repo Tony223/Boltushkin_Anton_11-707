e1e35c622dc3cd4efb9edbd5b38ecedb5b7a523e
Boltushkin Anton <32344640+Tony223@users.noreply.github.com>
2017-10-14 23:59:34 +0300

    Add files via upload

---
+++ Fall 2017/PS/PS1/Part4_5.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Part4_5
+{
+    class Program
+    {
+        static void Main()
+        {
+            int max = 1000000, count = 0;
+
+            for (int i = 0; i < max; i++)
+            {
+                int sum = 0;
+                if (i < 2)
+                    i = 1;
+
+                for (int j = 1; j < i; j++)
+                {
+                    if (i % j == 0)
+                        sum += j;
+                }
+                if (i == sum)
+                {
+                    count += 1;
+                    Console.WriteLine($"{i} - совершенное число");
+                }
+            }
+                Console.WriteLine($"Количество совершенных чисел = {count}");             
+        }
+    }
+}
