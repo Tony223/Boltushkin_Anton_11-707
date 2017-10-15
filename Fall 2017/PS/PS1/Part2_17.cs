7e5d8ed12e2deeefed4a043b7e7ba2d1819c4763
Boltushkin Anton <32344640+Tony223@users.noreply.github.com>
2017-10-15 19:32:05 +0300

    Add files via upload

---
+++ Fall 2017/PS/PS1/Part2_17.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Part2_17
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите n");
+            int original = int.Parse(Console.ReadLine());
+            int i = 2, maxLength = 0, a = 0;
+
+            while (i < 11)
+            {
+                int n = original, length = 1, res1 = i, res2 = 0;
+
+                while (n/i != 0)
+                {
+                    res2 = n % i;
+
+                    if (res2 <= res1)
+                    {
+                        res1 = res2;
+                        n /= i;
+                        length++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                if (n <= res1 && maxLength <= length && n / i == 0)
+                {
+                    maxLength = length;
+                    a = i;
+                    Console.WriteLine($"Длина самой длинной неубывающая цепочки цифр = {maxLength}, в {a}-ичной системе счисления.");
+                }
+                else
+                {
+                    a = i;
+                    Console.WriteLine($"Такой цепочки в {a}-ичной системе счисления нет или она недостаточно длинная.");
+                }
+                  
+                i++;
+            }
+        }
+    }
+}
