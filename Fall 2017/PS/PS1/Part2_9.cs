e4511c049fe116a9ee0a81c3a8878311379b2b4f
Boltushkin Anton <32344640+Tony223@users.noreply.github.com>
2017-10-15 19:36:00 +0300

    Add files via upload

---
+++ Fall 2017/PS/PS1/Part2_9.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Part2_9
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите число");
+            int number = int.Parse(Console.ReadLine());
+            int residue, count = 0;
+
+
+            while(number > 0)
+            {
+                residue = number % 2;
+                number = number / 2;
+                count = residue + count;
+
+            }
+            Console.WriteLine($"Число единиц = {count}");
+        }
+    }
+}
