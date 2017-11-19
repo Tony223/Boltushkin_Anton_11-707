8f96f4e1c83f52e23e585a6dda9c99e0b7cdf56a
Boltushkin Anton <32344640+Tony223@users.noreply.github.com>
2017-11-19 23:57:48 +0300

    Add files via upload

---
+++ Fall 2017/PS/PS2/Part1_16.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Part1_16
+{
+    class Program
+    {        
+        public static void Calculation(double eps)
+        {
+            int n = 0;
+            double g = 1, count = 0;
+
+            while (Math.Abs(g) >= eps)
+            {
+                if (n % 2 == 0)
+                    g = 1.0 / ((2 * n + 1) * (2 * n + 1));
+                else
+                    g = -1.0 / ((2 * n + 1) * (2 * n + 1));
+                n++;
+                count += g;
+            }
+            Console.WriteLine($"Number of steps = {n}");
+            Console.WriteLine($"G = {count}");
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("Enter the epsilon");
+            double eps = double.Parse(Console.ReadLine());
+            Calculation(eps);
+        }
+    } 
+}
